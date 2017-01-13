07d9bef6a38552e810aa098df6eda7e9a3d8fdeb
Waltsuva <waltsu.valtonen@hotmail.com>
2017-01-13 15:56:24 +0200

    Implementing initial stuff for AI

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,7 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by agnie on 1/12/2017.
  */
 public class Player {
+
+    private ArrayList<ArrayList<Integer>> movePool = new ArrayList<>();
+
+    private void addInitialMoves(){
+        ArrayList<Integer> coordinates = new ArrayList<>();
+        coordinates.add(4);
+        coordinates.add(4);
+
+        coordinates.add(4);
+        coordinates.add(16);
+
+        coordinates.add(16);
+        coordinates.add(4);
+
+        coordinates.add(16);
+        coordinates.add(16);
+
+        movePool.add(coordinates);
+    }
 }
